4378d45ebb2dacbb20301da3b5d35594823c2f69
chengbotao <chengbotao5221@163.com>
2022-06-02 15:36:29 +0800

    icon 图标

--- docs/.vuepress/config/site.ts
+++ docs/.vuepress/config/site.ts
@@ -2,8 +2,8 @@
  * @Description: 文件描述
  * @Author: Chengbotao
  * @Date: 2022-05-05 23:31:13
- * @LastEditors: Chengbotao
- * @LastEditTime: 2022-05-22 10:41:12
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-06-02 15:36:10
  * @FilePath: \vuepress-docs\docs\.vuepress\config\site.ts
  */
 import { SiteLocaleConfig, UserConfig } from "vuepress";
@@ -28,5 +28,5 @@ export const SiteConfig: UserConfig = {
   locales,
   // 站点顶层配置
   base: "/chengbotao/",
-  head: [["link", { rel: "icon", href: "/chengbotao.jpg" }]],
+  head: [["link", { rel: "icon", href: "/chengbotao.ico" }]],
 };
