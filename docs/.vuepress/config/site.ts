769a66df4dd2a104f12a2c780caf576e0e621fbf
chengbotao <chengbotao5221@163.com>
2023-02-27 15:55:29 +0800

    update config site

--- docs/.vuepress/config/site.ts
+++ docs/.vuepress/config/site.ts
@@ -3,7 +3,7 @@
  * @Author: Chengbotao
  * @Date: 2022-05-05 23:31:13
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-02-27 15:35:21
+ * @LastEditTime: 2023-02-27 15:55:09
  * @FilePath: \vuepress-docs\docs\.vuepress\config\site.ts
  */
 import { SiteLocaleConfig, UserConfig } from "vuepress";
@@ -32,7 +32,7 @@ export const SiteConfig: UserConfig = {
     ["link", { rel: "icon", href: `chengbotao/chengbotao.ico` }],
     ["meta", { property: "og:type", content: "website" }],
     ["meta", { property: "og:title", content: "Botaoxy" }],
-    ["meta", { property: "og:description", content: "一切尽意,万事从换！！！" }],
+    ["meta", { property: "og:description", content: "寻找，自己生活态度！️✌️ 拒绝标签，追求自我！👍 学习，摄影绘画理财！🤜 一切尽意，万事从欢！🤟" }],
     ["meta", { property: "og:image", content: "https://chengbotao.github.io/chengbotao/chengbotao.png" }],
     ["meta", { property: "og:url", content: "https://chengbotao.github.io/chengbotao" }]
   ],
